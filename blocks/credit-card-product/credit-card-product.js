833b35c0a0c175629f415934dd193d4bae197d75
sltamula <stamula@deloitte.com>
2025-09-16 15:09:36 +0800

    update file

--- blocks/credit-card-product/credit-card-product.js
+++ blocks/credit-card-product/credit-card-product.js
@@ -2,7 +2,7 @@ export default async function decorate(block) {
   const fragmentPath = block.textContent.trim();
 
   // Correct: Construct the full URL for the persisted query, including variables.
-  const url = `https://author-p9606-e71941.adobeaemcloud.com/graphql/execute.json/miniproject/getCreditCardDetails;path=${(fragmentPath)}`;
+  const url = `https://author-p9606-e71941.adobeaemcloud.com/graphql/execute.json/miniproject/getCreditCardDetails;path=${fragmentPath}`;
 
   try {
     const response = await fetch(url);
@@ -11,7 +11,52 @@ export default async function decorate(block) {
     }
     const data = await response.json();
     console.log(data);
-    return data.creditCardContainerByPath.item; // Access data based on the response structure
+    const ccDetails = data.creditCardContainerByPath.item;
+
+    block.innerHTML = ''; // Clear existing content
+
+    // Render the fetched data
+    if (ccDetails.length > 0) {
+      ccDetails.forEach(ccDetail => {
+        const cardContainer = document.createElement('div');
+        cardContainer.className = 'credit-card__container';
+
+        const cardName = document.createElement('h1');
+        cardName.textContent = ccDetail.name;
+        cardName.className = 'credit-card__name';
+
+        const cardImage = document.createElement('img');
+        cardImage.src = ccDetail.image['_path'];
+        cardImage.className = 'credit-card__image';
+
+        const cardHeader = document.createElement('div');
+        cardHeader.textContent = ccDetail.customHeader;
+        cardHeader.className = 'credit-card__header';
+
+        const cardDescription = document.createElement('div');
+        cardDescription.textContent = ccDetail.description.html;
+        cardDescription.className = 'credit-card__description';
+
+        const cardFeatures = document.createElement('div');
+        cardFeatures.textContent = ccDetail.cardFeatures.html;
+        cardFeatures.className = 'credit-card__features';
+
+        const cardBenefits = document.createElement('div');
+        cardBenefits.textContent = ccDetail.extraBenefits.html;
+        cardBenefits.className = 'credit-card__benfits';
+
+        cardContainer.append(
+          cardName,
+          cardImage,
+          cardHeader,
+          cardFeatures,
+          cardBenefits,
+          cardDescription
+        );
+
+        block.appendChild(cardContainer);
+      });
+    }
   } catch (error) {
     console.error('Error fetching data:', error);
     return null; // or throw an error
