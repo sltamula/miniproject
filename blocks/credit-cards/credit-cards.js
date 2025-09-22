209c9a4beeb946bd9b54f03cf41eae325d1003a5
sltamula <stamula@deloitte.com>
2025-09-22 18:57:37 +0800

    updated code

--- blocks/credit-cards/credit-cards.js
+++ blocks/credit-cards/credit-cards.js
@@ -9,12 +9,12 @@ export default function decorate(block) {
     const li = document.createElement('div');
     moveInstrumentation(row, li);
     while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach(divContainer => {
-      if (divContainer.children.length === 1) {
-        // const parentDiv = document.querySelector('div');
-        // const cfPath = parentDiv.querySelector('a')?.title;
-      }
-    });
+    // [...li.children].forEach(divContainer => {
+    //   if (divContainer.children.length === 1) {
+    //     // const parentDiv = document.querySelector('div');
+    //     // const cfPath = parentDiv.querySelector('a')?.title;
+    //   }
+    // });
   });
   block.textContent = '';
   block.append(cardContainer);
