a837680d99ccdb136bfa98f2221c0e594f8984bd
sltamula <stamula@deloitte.com>
2025-09-22 13:51:00 +0800

    updated code

--- blocks/credit-cards/credit-cards.js
+++ blocks/credit-cards/credit-cards.js
@@ -1,95 +1,27 @@
-const API_ENDPOINT =
-  'https://author-p9606-e71941.adobeaemcloud.com/graphql/execute.json/miniproject/getCreditCardDetails';
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
 
-/**
- * Entry point for the block decoration.
- * @param {HTMLElement} block The block element.
- */
-export default async function decorate(block) {
-  const children = [...block.children];
-  const cfPath = children[0]?.querySelector('a')?.title;
-  const authoredButtonText = children[1]?.textContent.trim();
-  const authoredLink = children[1]?.querySelector('a')?.href;
-
-  if (!cfPath) {
-    console.error('Content fragment path not found.');
-    return;
-  }
-
-  const url = `${API_ENDPOINT};path=${cfPath}`;
-
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const cfData = await response.json();
-    const ccDetails = cfData?.data?.creditCardContainerByPath?.item;
-
-    if (ccDetails) {
-      block.innerHTML = '';
-      const cardContainer = document.createElement('div');
-      cardContainer.className = 'credit-card__container';
-
-      const cardBodyContainer = document.createElement('div');
-      cardBodyContainer.className = 'credit-card__body';
-
-      // Create and append card details
-      const elementsToCreate = [
-        {
-          tag: 'img',
-          src: ccDetails.image._path,
-          className: 'credit-card__image',
-        },
-        {
-          tag: 'h3',
-          textContent: ccDetails.name,
-          className: 'credit-card__name',
-        },
-        {
-          tag: 'div',
-          innerHTML: ccDetails.description.html,
-          className: 'credit-card__description',
-        },
-        {
-          tag: 'div',
-          innerHTML: ccDetails.cardFeatures.html,
-          className: 'credit-card__features',
-        },
-        {
-          tag: 'div',
-          innerHTML: ccDetails.cardBenefits.html,
-          className: 'credit-card__benefits',
-        },
-      ];
-
-      elementsToCreate.forEach(
-        ({ tag, src, textContent, innerHTML, className }) => {
-          const element = document.createElement(tag);
-
-          if (src) element.src = src;
-          if (textContent) element.textContent = textContent;
-          if (innerHTML) element.innerHTML = innerHTML;
-          if (className) element.className = className;
-
-          // Append the image to the main container, everything else to the body
-          if (tag === 'img') {
-            cardContainer.appendChild(element);
-          } else {
-            cardBodyContainer.appendChild(element);
-          }
-        }
-      );
-
-      // Append the populated card body to the main card container
-      cardContainer.appendChild(cardBodyContainer);
-
-      // Append the card container to the block
-      block.appendChild(cardContainer);
-    } else {
-      console.error('No credit card details found in content fragment.');
-    }
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
+export default function decorate(block) {
+  /* change to ul, li */
+  const ul = document.createElement('ul');
+  [...block.children].forEach(row => {
+    const li = document.createElement('li');
+    moveInstrumentation(row, li);
+    while (row.firstElementChild) li.append(row.firstElementChild);
+    [...li.children].forEach(div => {
+      if (div.children.length === 1 && div.querySelector('picture'))
+        div.className = 'cards-card-image';
+      else div.className = 'cards-card-body';
+    });
+    ul.append(li);
+  });
+  ul.querySelectorAll('picture > img').forEach(img => {
+    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [
+      { width: '750' },
+    ]);
+    moveInstrumentation(img, optimizedPic.querySelector('img'));
+    img.closest('picture').replaceWith(optimizedPic);
+  });
+  block.textContent = '';
+  block.append(ul);
 }
