2a6b46b56c39a83d6ecf224ac0c6e85456d3e20d
sltamula <stamula@deloitte.com>
2025-09-22 16:51:38 +0800

    updated code

--- blocks/credit-cards/credit-cards.js
+++ blocks/credit-cards/credit-cards.js
@@ -1,63 +1,8 @@
 function renderContent(block, ccDetails) {
-  block.innerHTML = '';
-
-  // Create card container
-  const cardContainer = document.createElement('div');
-  cardContainer.className = 'credit-card__container';
-
-  const cardBodyContainer = document.createElement('div');
-  cardBodyContainer.className = 'credit-card__body';
-
-  // Create and append card details
-  const elementsToCreate = [
-    { tag: 'img', src: ccDetails.image._path, className: 'credit-card__image' },
-    { tag: 'h3', textContent: ccDetails.name, className: 'credit-card__name' },
-    {
-      tag: 'div',
-      innerHTML: ccDetails.description.html,
-      className: 'credit-card__description',
-    },
-    {
-      tag: 'div',
-      innerHTML: ccDetails.cardFeatures.html,
-      className: 'credit-card__features',
-    },
-    {
-      tag: 'div',
-      innerHTML: ccDetails.cardBenefits.html,
-      className: 'credit-card__benefits',
-    },
-  ];
-
-  elementsToCreate.forEach(
-    ({ tag, src, textContent, innerHTML, className }) => {
-      const element = document.createElement(tag);
-
-      if (src) element.src = src;
-      if (textContent) element.textContent = textContent;
-      if (innerHTML) element.innerHTML = innerHTML;
-      if (className) element.className = className;
-
-      // Append the image to the main container, everything else to the body
-      if (tag === 'img') {
-        cardContainer.appendChild(element);
-      } else {
-        cardBodyContainer.appendChild(element);
-      }
-    }
-  );
-
-  // Append the populated card body to the main card container
-  cardContainer.appendChild(cardBodyContainer);
-
-  // Handle button generation
-  // createButton(cardContainer, authoredLink, authoredButtonText);
-
-  // Append the card container to the block
-  block.appendChild(cardContainer);
+  return null;
 }
 
-async function getContentFragmentDetails(cfPath, block, children) {
+async function getContentFragmentDetails(cfPath, block) {
   let url;
   const API_ENDPOINT =
     'https://author-p9606-e71941.adobeaemcloud.com/graphql/execute.json/miniproject/getCreditCardDetails';
@@ -93,7 +38,7 @@ export default function decorate(block) {
   const children = [...block.children];
   children.forEach(row => {
     const cfPath = children[0].querySelector('a')?.title;
-    getContentFragmentDetails(cfPath, block, children);
+    getContentFragmentDetails(cfPath, block);
   });
   block.textContent = '';
   block.append(ul);
