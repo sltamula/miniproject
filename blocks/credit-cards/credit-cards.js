1f0ff19a1c71fbdb1dbd749549e297ddd3d2ad20
sltamula <stamula@deloitte.com>
2025-09-22 16:52:16 +0800

    updated code

--- blocks/credit-cards/credit-cards.js
+++ blocks/credit-cards/credit-cards.js
@@ -1,37 +1,3 @@
-function renderContent(block, ccDetails) {
-  return null;
-}
-
-async function getContentFragmentDetails(cfPath, block) {
-  let url;
-  const API_ENDPOINT =
-    'https://author-p9606-e71941.adobeaemcloud.com/graphql/execute.json/miniproject/getCreditCardDetails';
-
-  if (cfPath !== null && cfPath !== '') {
-    url = `${API_ENDPOINT};path=${cfPath}`;
-  } else {
-    console.error('Content fragment path not found.');
-    return;
-  }
-
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const cfData = await response.json();
-    const ccDetails = cfData?.data?.creditCardContainerByPath?.item;
-
-    if (ccDetails) {
-      renderContent(block, ccDetails);
-    } else {
-      console.error('No credit card details found in content fragment.');
-    }
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
-}
-
 export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
