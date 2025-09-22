c5c0891bef2bae7afe8578f853f0565def2ff38e
sltamula <stamula@deloitte.com>
2025-09-22 21:44:15 +0800

    updated code

--- blocks/credit-cards/credit-cards.js
+++ blocks/credit-cards/credit-cards.js
@@ -1,11 +1,33 @@
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
+async function getContentFragmentDetails(cfPath) {
+  const API_ENDPOINT =
+    'https://author-p9606-e71941.adobeaemcloud.com/graphql/execute.json/miniproject/getCreditCardDetails';
+
+  if (cfPath) {
+    const url = `${API_ENDPOINT};path=${cfPath}`;
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const cfData = await response.json();
+      const ccDetails = cfData?.data?.creditCardContainerByPath?.item;
+
+      if (ccDetails) {
+        //renderContent(ccDetails);
+        console.log('Hello');
+      } else {
+        console.error('No credit card details found in content fragment.');
+      }
+    }
+  }
+}
+
 export default function decorate(block) {
   /* change to ul, li */
   const cardContainer = document.createElement('div');
   cardContainer.className = 'credit-card__container';
 
-  [...block.children].forEach(child => {
+  [...block.children].forEach(row => {
     const li = document.createElement('div');
     moveInstrumentation(child, li);
     while (row.firstElementChild) li.append(row.firstElementChild);
@@ -13,6 +35,7 @@ export default function decorate(block) {
       if (divContainer.children.length === 1) {
         const parentDiv = document.querySelector('div');
         const cfPath = parentDiv.querySelector('a')?.title;
+        getContentFragmentDetails(cfPath);
       }
     });
     cardContainer.append(li);
