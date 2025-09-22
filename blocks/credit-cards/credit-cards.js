fba1b709bc47164ff288d8eca1e824af9284564d
sltamula <stamula@deloitte.com>
2025-09-22 17:32:22 +0800

    updated code

--- blocks/credit-cards/credit-cards.js
+++ blocks/credit-cards/credit-cards.js
@@ -8,12 +8,12 @@ export default function decorate(block) {
   [...block.children].forEach(row => {
     const li = document.createElement('div');
     moveInstrumentation(row, li);
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach(div => {
-      if (div.children.length === 1) {
-        console.log('Hello');
-      }
-    });
+    // while (row.firstElementChild) li.append(row.firstElementChild);
+    // [...li.children].forEach(div => {
+    //   if (div.children.length === 1) {
+    //     console.log('Hello');
+    //   }
+    // });
     cardContainer.append(li);
   });
   block.textContent = '';
