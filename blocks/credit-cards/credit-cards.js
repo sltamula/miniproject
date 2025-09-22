7b3d1cf10858e6f45d2edb7d3ba3f2a6d4c38e76
sltamula <stamula@deloitte.com>
2025-09-22 17:03:32 +0800

    updated code

--- blocks/credit-cards/credit-cards.js
+++ blocks/credit-cards/credit-cards.js
@@ -3,10 +3,10 @@ import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
   /* change to ul, li */
-  const ul = document.createElement('div');
+  const ul = document.createElement('ul');
   [...block.children].forEach(row => {
-    const li = document.createElement('div');
-    //moveInstrumentation(row, li);
+    const li = document.createElement('li');
+    moveInstrumentation(row, li);
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach(div => {
       if (div.children.length === 1 && div.querySelector('picture'))
